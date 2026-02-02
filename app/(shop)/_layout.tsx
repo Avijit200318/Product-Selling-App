31de1e73df2f665a5d1f41c4c513927cd1686312
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2026-02-02 10:58:28 +0530

    styled the tab bar in our app

--- app/(shop)/_layout.tsx
+++ app/(shop)/_layout.tsx
@@ -1,22 +1,38 @@
 import Ionicons from "@expo/vector-icons/Ionicons"
 import { Tabs } from "expo-router"
 import { SafeAreaView } from "react-native-safe-area-context"
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import FontAwesome6 from '@expo/vector-icons/FontAwesome6';
 
 const TabsLayout = () => {
     return (
-        <SafeAreaView className="flex-1">
-            <Tabs>
+        <SafeAreaView edges={["top"]} className="flex-1">
+            <Tabs screenOptions={{
+                tabBarActiveTintColor: "#1BC464",
+                tabBarInactiveTintColor: "gray",
+                tabBarLabelStyle: {fontSize: 14},
+
+                tabBarStyle: {
+                    borderTopLeftRadius: 20,
+                    borderTopRightRadius: 20,
+                    paddingTop: 10
+                }
+            }}>
                 <Tabs.Screen
                     name="index"
                     options={{
-                        title: "index",
+                        title: "Shop",
                         headerShown: false,
-                        tabBarIcon: () => <Ionicons name="home" size={28} />
+                        tabBarIcon: ({color}) => <FontAwesome name="shopping-cart" size={24} color={color} />
                     }}
                 />
                 <Tabs.Screen
                     name="orders"
-                    options={{ title: "orders", headerShown: false }}
+                    options={{
+                        title: "orders",
+                        headerShown: false,
+                        tabBarIcon: ({color}) => <FontAwesome6 name="box" size={24} color={color} />
+                    }}
                 />
             </Tabs>
         </SafeAreaView>
