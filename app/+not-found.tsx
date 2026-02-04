5855bc71767b5f23582993fe1c669f6dcb55b74e
Avijit Hira <136997678+Avijit200318@users.noreply.github.com>
2026-02-04 12:31:54 +0530

    added route not found page

---
+++ app/+not-found.tsx
@@ -0,0 +1,29 @@
+import { View, StyleSheet, Text } from 'react-native';
+import { Link, Stack } from 'expo-router';
+
+export default function NotFoundScreen() {
+  return (
+    <>
+      <Stack.Screen options={{ title: 'Oops! Not Found' }} />
+      <View style={styles.container}>
+        <Text className='text-3xl mb-4 font-semibold'>404 not found</Text>
+        <Link href="/" style={styles.button}>
+          Go back to Home screen!
+        </Link>
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+  },
+});
